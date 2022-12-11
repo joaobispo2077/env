f9c11823307e064fc064cfab775c10fb902b40b3
João Bispo <joaobispo2077@gmail.com>
2022-12-11 19:20:25 -0300

    refactor: separe script reponsibility

--- windows/bootstrap.ps1
+++ windows/bootstrap.ps1
@@ -1,5 +1,4 @@
 # Store initial time
-
 $initialTime = date
 
 # Install package manager chocolatey - https://chocolatey.org/install
@@ -7,8 +6,16 @@ echo "Installing chocolatey..."
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Basic apps
-echo "Installing basic apps (navigator, useful tools)..."
-choco install opera-gx vlc adobereader discord lightshot notion brave firefox steam obs-studio -y
+echo "Installing basic apps (navigator, read pdf, talk, useful tools)..."
+choco install opera-gx vlc adobereader discord lightshot notion brave firefox -y
+
+# Media content apps
+echo "Installing media content apps (video & image creation)..."
+choco install obs-studio 4k-video-downloader gimp photogimp -y
+
+# Fun apps
+echo "Installing fun apps (gaming)..."
+choco install steam cemu -y
 
 # Optinal benchmark tools
 # echo "Installing benchmark tools..."
@@ -16,7 +23,7 @@ choco install opera-gx vlc adobereader discord lightshot notion brave firefox st
 
 # Dev tools
 echo "Dev tools basic apps (navigator, useful tools)..."
-choco install jetbrainsmono git microsoft-windows-terminal vscode vscode-settingssync nvm pyenv-win figma insomnia-rest-api-client beekeeper-studio dbeaver docker-desktop -y
+choco install jetbrainsmono git microsoft-windows-terminal tldr vscode vscode-settingssync nvm pyenv-win figma insomnia-rest-api-client beekeeper-studio dbeaver docker-desktop -y
 
 # Show initial and final time of execution
 echo "Started at:"
