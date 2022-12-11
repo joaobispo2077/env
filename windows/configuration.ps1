b859915dd2e9ac83caa65b5e37a8cc098e505d54
João Bispo <joaobispo2077@gmail.com>
2022-12-11 14:20:35 -0300

    feat: add programming language version config for nodejs and python

---
+++ windows/configuration.ps1
@@ -0,0 +1,18 @@
+# Add node.js versions with LTS
+nvm version
+nvm on
+nvm list
+nvm install 14
+nvm install 16
+nvm install lts
+node -v
+
+# Add python LTS
+pyenv install -list
+pyenv install 3.10.0
+pyenv versions
+pyenv global 3.10.0
+python -V
+
+# Refresh environment variables from PATH
+refreshenv
