892edf2d5619a8b9fa18cc06bc67564a8cd32760
João Bispo <joaobispo2077@gmail.com>
2023-08-30 16:53:04 -0300

    chore: add nvm config append on ~/.zshrc

--- ubuntu/scripts/install-nvm.sh
+++ ubuntu/scripts/install-nvm.sh
@@ -6,6 +6,17 @@ sudo apt update
 # Install Node Version Manager (NVM)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
+nvm_config="
+export NVM_DIR=\"\$HOME/.nvm\"
+[ -s \"\$NVM_DIR/nvm.sh\" ] && \\. \"\$NVM_DIR/nvm.sh\"  # This loads nvm
+[ -s \"\$NVM_DIR/bash_completion\" ] && \\. \"\$NVM_DIR/bash_completion\"  # This loads nvm bash_completion
+"
+
+# Append NVM configuration to ~/.zshrc
+echo "$nvm_config" >> ~/.zshrc
+
+echo "NVM configuration has been appended to ~/.zshrc"
+
 # Load NVM into the current shell session
 source ~/.nvm/nvm.sh
 
@@ -20,3 +31,5 @@ echo "Git $(git --version)"
 echo "NVM $(nvm --version)"
 echo "Node.js $(node --version)"
 echo "npm $(npm --version)"
+
+source ~/.zshrc
