058dd916f1014c2abc64bb3ea1a48c2193004dc5
João Bispo <joaobispo2077@gmail.com>
2023-09-22 11:15:29 -0300

    feat: set line break between plugins at install-plugins-zinit.sh

--- ubuntu/scripts/install-plugins-zinit.sh
+++ ubuntu/scripts/install-plugins-zinit.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-plugins_config="
-zinit wait lucid light-mode for atinit'zicompinit; zicdreplay'
-zdharma-continuum/fast-syntax-highlighting
-atload'_zsh_autosuggest_start'
-zsh-users/zsh-autosuggestions
-blockf atpull'zinit creinstall -q .'
-zsh-users/zsh-completions
-"
+plugins_config=(
+"zinit wait lucid light-mode for"
+"  atinit'zicompinit; zicdreplay'"
+"  zdharma-continuum/fast-syntax-highlighting"
+"  atload'_zsh_autosuggest_start'"
+"  zsh-users/zsh-autosuggestions"
+"  blockf atpull'zinit creinstall -q .'"
+"  zsh-users/zsh-completions"
+)
+
+# Join the array elements into a single string
+plugins_config_string=$(printf "%s\n" "${plugins_config[@]}")
 
 # Append plugins configuration to ~/.zshrc
-echo "$plugins_config" >> ~/.zshrc
+echo "$plugins_config_string" >> ~/.zshrc
 
 echo "Plugins have been appended to ~/.zshrc"
